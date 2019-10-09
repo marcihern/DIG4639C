d6e860934288ccfb212f7c77d84afb8326f587d3
Marcela Hernandez <mhern890@knights.ucf.edu>
2019-10-09 19:09:01 -0400

    lab 3 - basic react app

---
+++ lab3/src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css"
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div>
+        <Card content="This is a card!"></Card>
+      </div>
+    );
+  }
+}
+
+
+export default Card;
